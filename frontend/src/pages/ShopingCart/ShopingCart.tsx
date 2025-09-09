07090deaf1f876818af57e2434f7c768a2ab6666
bohdan <mota82589@gmail.com>
2025-09-09 20:25:26 +0300

    changes everything

---
+++ frontend/src/pages/ShopingCart/ShopingCart.tsx
@@ -0,0 +1,12 @@
+import type { FC } from "react";
+import Navigation from "../../Components/Navigation";
+
+const ShoppingCart: FC = () => {
+  return (
+    <>
+      <Navigation />
+    </>
+  );
+};
+
+export default ShoppingCart;
